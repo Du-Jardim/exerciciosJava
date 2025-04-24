12316d6e154538344b4e2f02514db23eae420d50
Du-Jardim <maria.sousa437@etec.sp.gov.br>
2025-04-24 18:16:55 -0300

    atualizacao
    atualizacao metodo com retorno

--- src/MetodoComParametros.java
+++ src/MetodoComParametros.java
@@ -1,1 +1,14 @@
-public class MetodoComRetorno{}
+import java.util.*;
+
+public class MetodoComParametros {
+  public static void main(String[] args) {
+    System.out.println(mostrarMensagem());
+  }
+
+  public static String mostrarMensagem(){
+    Scanner  = new Scanner(System.in);
+    System.out.println("Digite a mensagem: ");
+    return v.nextLine();
+  }
+
+}
