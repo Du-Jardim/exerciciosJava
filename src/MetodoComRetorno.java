8b7bd8bc36efc885d7b7d48dec5226de5a744bf6
Du-Jardim <maria.sousa437@etec.sp.gov.br>
2025-04-24 17:31:06 -0300

    24.04.2025
    Commit da segunda parte da atividade

--- src/MetodoComRetorno.java
+++ src/MetodoComRetorno.java
@@ -1,5 +1,29 @@
 public class MetodoComRetorno {
     public static void main(String[] args) {
+        System.out.println("Ano atual: " + Ano());
+        System.out.println("Nome Completo: " + NomeCompleto());
+        System.out.println("Valor fixo: " + valor());
+        System.out.println("V || F " + alternativa());
+        System.out.println("O valor de PI é: " + Pi());
+    }
 
+    public static int Ano(){
+        return java.time.Year.now().getValue();
+    }
+
+    public static String NomeCompleto(){
+        return "Maria Eduarda Jardim Sousa";
+    }
+
+    public static int valor(){
+        return 33;
+    }
+
+    public static boolean alternativa(){
+        return true;
+    }
+
+    public static float Pi(){
+        return 3.14159F;
     }
 }
